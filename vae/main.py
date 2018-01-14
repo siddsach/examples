406a70ef27eb4f40ce69704780f9dea81c494a76
siddsach <ssachdeva@uchicago.edu>
2018-01-14 04:24:18 -0600

    changed KL loss from exp(std) to std^2

--- vae/main.py
+++ vae/main.py
@@ -86,7 +86,7 @@ def loss_function(recon_x, x, mu, logvar):
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.pow(2))
     # Normalise by same number of elements as in reconstruction
     KLD /= args.batch_size * 784
 
